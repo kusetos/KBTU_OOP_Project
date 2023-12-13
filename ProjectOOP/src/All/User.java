628373fd38e5080191319ab41d8f6becaa243151
Arnur <a_tanirbergenov@kbtu.kz>
2023-12-13 22:04:56 +0600

    Wrote a bit in User class

--- ProjectOOP/src/All/User.java
+++ ProjectOOP/src/All/User.java
@@ -1,5 +1,59 @@
 package All;
 
+import java.util.Objects;
 public class User {
+	String username;
+	String password;
+	String birthDate;
+	String phoneNumber;
+	String email;
 	String name;
+	String surname;
+	
+	public User(String username, String password, String birthDate, String phoneNumber, String email, String name, String surname) {
+		this.birthDate = birthDate;
+		this.password = password;
+		this.email = email;
+		this.phoneNumber = phoneNumber;
+		this.surname = surname;
+		this.username = username;
+	}
+	public User(String username, String password) {
+		this.password = password;
+		this.username = username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public void changePassword(String oldPassword, String newPassword) {
+		if(this.password.equals(oldPassword)) {
+			this.password = newPassword;
+			System.out.println("Password changed successfully");
+		}
+		else {
+			System.out.println("Wrong password");
+		}
+	}
+	
+	public boolean equals(Object o){
+		if(this == o)return true;
+		if(o == null)return false;
+		if(this.getClass() != o.getClass())return false;
+		User u = (User)o;
+		if(this.hashCode() != u.hashCode())return false;
+		return Objects.equals(this.birthDate, u.birthDate) && Objects.equals(this.email, u.email)
+				&& Objects.equals(this.name, u.name) && Objects.equals(this.password, u.password)
+				&& Objects.equals(this.surname, u.surname) && Objects.equals(this.username, u.username);
+	}
+	
+	public int hashCode() {
+		return Objects.hash(username, birthDate, email, name, surname,password);
+	}
+	
 }
