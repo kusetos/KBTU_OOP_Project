7a00ac4cb6905ac335665ef50d46fa1172d956ff
kusetos <itokpak2004@gmail.com>
2023-12-16 16:53:44 +0600

    minimal

--- ProjectOOP/src/People/BachelorStudent.java
+++ ProjectOOP/src/People/BachelorStudent.java
@@ -1,31 +1,37 @@
 package People;
 
+import java.util.Date;
+
 import Entities.DiplomaProject;
+import Enumerators.Faculty;
 
 /**
 * @generated
 */
 public class BachelorStudent extends Student {
-    
-    /**
-    * @generated
-    */
+
     private DiplomaProject diplomaProject;
     
-    
-    
+    public BachelorStudent() {
+		super();
+	}
 
-    /**
-    * @generated
-    */
-    private DiplomaProject getDiplomaProject() {
+
+	public BachelorStudent(String username, String password, Date birthDate, String phoneNumber, String email,
+			String name, String surname, String id, int yearOfStudy, double gpa, int creditLimit, Faculty faculty,
+			String speciality) {
+		super(username, password, birthDate, phoneNumber, email, name, surname, id, yearOfStudy, gpa, creditLimit, faculty,
+				speciality);
+
+	}
+
+
+	public DiplomaProject getDiplomaProject() {
         return this.diplomaProject;
     }
     
-    /**
-    * @generated
-    */
-    private DiplomaProject setDiplomaProject(DiplomaProject diplomaProject) {
+
+    public void setDiplomaProject(DiplomaProject diplomaProject) {
         this.diplomaProject = diplomaProject;
     }
     
