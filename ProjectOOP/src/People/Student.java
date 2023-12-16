9efc7847e26efd4c1c9d6a090ec6a6e5db1d7825
kusetos <itokpak2004@gmail.com>
2023-12-16 17:32:14 +0600

    yes
    View Courses, Register for Courses
    o View info about teacher of a specific course
    o View Marks
    o View Transcript
    o Rate teachers
    o Get Transcript
    o Student organizations

--- ProjectOOP/src/People/Student.java
+++ ProjectOOP/src/People/Student.java
@@ -1,9 +1,12 @@
 package People;
+import java.util.Date;
+
 import java.util.Objects;
 import java.util.Vector;
 
 import Entities.Course;
 import Enumerators.Faculty;
+import Enumerators.Organization;
 public class Student extends User{
 	String id;
 	int yearOfStudy;
@@ -13,18 +16,14 @@ public class Student extends User{
 	int creditLimit;
 	Faculty faculty;
 	String speciality;
+	Organization org;
 	
 	public Student() {
-		this.yearOfStudy = 0;
-		this.gpa = 0;
-		this.creditLimit = 0;
-		this.faculty = null;
-		this.speciality = null;
-		this.id = null;
+		super();
 	}
 	
-	public Student(String username, String password, String birthDate, String phoneNumber, String email, String name,
-			String surname, String id, int yearOfStudy, double gpa, int creditLimit, Faculty faculty, String speciality) {
+	public Student(String username, String password, Date birthDate, String phoneNumber, String email, String name,
+			String surname, String id, int yearOfStudy, double gpa, int creditLimit, Faculty faculty, String speciality, Organization org) {
 		super(username, password, birthDate, phoneNumber, email, name, surname);
 		this.id = id;
 		this.yearOfStudy = yearOfStudy;
@@ -32,40 +31,100 @@ public class Student extends User{
 		this.creditLimit = creditLimit;
 		this.faculty = faculty;
 		this.speciality = speciality;
+		this.org = org;
 	}
 	
+
+		
+	public String getId() {
+		return id;
+	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
-	
-	public void setYearOfStudy(int yearOfStudy) {
-		this.yearOfStudy = yearOfStudy;		
+
+	public int getYearOfStudy() {
+		return yearOfStudy;
 	}
-	
+
+	public void setYearOfStudy(int yearOfStudy) {
+		this.yearOfStudy = yearOfStudy;
+	}
+
+	public int getTotalCredits() {
+		return totalCredits;
+	}
+
+	public void setTotalCredits(int totalCredits) {
+		this.totalCredits = totalCredits;
+	}
+
+	public double getGpa() {
+		return gpa;
+	}
+
+	public void setGpa(double gpa) {
+		this.gpa = gpa;
+	}
+
+	public int getCreditLimit() {
+		return creditLimit;
+	}
+
+	public void setCreditLimit(int creditLimit) {
+		this.creditLimit = creditLimit;
+	}
+
+	public Faculty getFaculty() {
+		return faculty;
+	}
+
 	public void setFaculty(Faculty faculty) {
 		this.faculty = faculty;
 	}
-	
+
+	public String getSpeciality() {
+		return speciality;
+	}
+
 	public void setSpeciality(String speciality) {
 		this.speciality = speciality;
 	}
 	
-	public String getSpeciality() {
-		return speciality;
+	public Organization getOrganization() {
+		return org;
+	}
+	public void setOrganization(Organization org) {
+		this.org = org;
 	}
 	
-	public Faculty getFaculty() {
-		return faculty;
+	public String viewCourses() {
+		String strCourses = new String();
+		for(Course c : courses) {
+			strCourses += c.toString();
+			strCourses += '\n';
+		}
+		return strCourses;
+	}
+	public void registerForCourses(Course course) {
+		manager.approveStudentRegistration(course);
+	}
+	public Teacher viewTeacherOfCourse(Course course) {
+		return course.getTeacher();
+	}
+	public Transcript getTranscript() {
+		return transcript;
+	}
+	public void rateTeachers(Teacher teacher, int rate) {
+		teacher.addRate(rate);
+	}
+	public Organization studentOrganization() {
+		return this.studentOrganization();
 	}
 	
-	public int getYearOfStudy() {
-		return yearOfStudy;
-	}
 	
-	public String getId() {
-		return id;
-	}
-	
+
 	public boolean equals(Object o) {
 		if(!super.equals(o))return false;
 		Student s = (Student)o;
