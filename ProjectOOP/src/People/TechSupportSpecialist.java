df0479709a9caff495cdccf27acaae1edac45f1f
godra-y <122892394+godra-y@users.noreply.github.com>
2023-12-19 21:53:55 +0600

    Update TechSupportSpecialist.java

--- ProjectOOP/src/People/TechSupportSpecialist.java
+++ ProjectOOP/src/People/TechSupportSpecialist.java
@@ -1,60 +1,44 @@
 package People;
 
+import java.util.Date;
+import java.util.Vector;
+import DataBase.Data;
 import Entities.Order;
 
-/**
-* @generated
-*/
 public class TechSupportSpecialist extends Employee {
     
+    public TechSupportSpecialist(String username, String password, Date birthDate, String phoneNumber, String email, String name, 
+			String surname, double Salary, Date hiredate) {
+		super(username, password, birthDate, phoneNumber, email, name, surname, Salary, hiredate);
+	}
     
-    /**
-    * @generated
-    */
-    private Order order;
-    
-    
-
-    
-    /**
-    * @generated
-    */
-    public Order getOrder() {
-        return this.order;
+    public void acceptOrder(Order order) {
+        Vector<Order> newOrders = Data.getOrders();
+        if (newOrders.contains(order)) {
+            newOrders.remove(order);
+            order.viewStatus();
+            System.out.println("The order has been accepted: " + order.getDescription());
+        } else {
+            System.out.println("This order is not in the list of new orders.");
+        }
     }
     
-    /**
-    * @generated
-    */
-    public Order setOrder(Order order) {
-        this.order = order;
+    public void rejectOrder(Order order) {
+        Vector<Order> newOrders = Data.getOrders();
+        if (newOrders.contains(order)) {
+            newOrders.remove(order);
+            System.out.println("Заказ отклонен: " + order.getDescription());
+        } else {
+            System.out.println("The order was rejected.");
+        }
     }
     
-    
-    
-
-    //                          Operations                                  
-    
-    /**
-    * @generated
-    */
-    public void acceptOrder() {
-        //TODO
-    }
-    
-    /**
-    * @generated
-    */
-    public void rejectOrder() {
-        //TODO
-    }
-    
-    /**
-    * @generated
-    */
     public void viewNewOrders() {
-        //TODO
+        Vector<Order> newOrders = Data.getOrders();
+        System.out.println("List of new orders:");
+        for (Order order : newOrders) {
+            System.out.println(order);
+        }
     }
-    
-    
 }
+
