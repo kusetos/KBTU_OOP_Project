c47c217d19285f7569308934a461d14040c8b90c
kusetos <itokpak2004@gmail.com>
2023-12-27 01:36:23 +0600

    student controla

---
+++ ProjectOOP/src/Controllers/SudentController.java
@@ -0,0 +1,26 @@
+package Controllers;
+
+import java.text.ParseException;
+
+public class SudentController {
+	public void mainMenu() throws ParseException {
+		System.out.flush();
+		System.out.println("---===KBTU UNIVERSITY SYSTEM===---");
+		System.out.println("---===========Student==========---\n");
+		System.out.println("Chose operation: \n");
+		System.out.println("1  :  View Courses");
+		System.out.println("2  :  Register for Courses");
+		System.out.println("3  :  View info about teacher of a specific course");
+		System.out.println("4  :  View Marks");
+		System.out.println("5  :  View Transcript");
+		System.out.println("6  :  Rate teachers");
+		System.out.println("7  :  Student organizations");
+		System.out.println("8  :  show Info");
+		System.out.println("9  :  calculateGPA");
+		System.out.println("10  :  more");
+		System.out.println("0  :  logging out");
+		
+		 
+		
+	}
+}
