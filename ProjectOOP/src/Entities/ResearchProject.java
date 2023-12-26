9e1c1ded255d5f484f46e8221195ad3941f99217
kusetos <itokpak2004@gmail.com>
2023-12-26 22:04:13 +0600

    diploma for graduate students

---
+++ ProjectOOP/src/Entities/ResearchProject.java
@@ -0,0 +1,71 @@
+package Entities;
+
+import People.*;
+import java.util.*;
+
+
+public class ResearchProject{
+    
+    
+	private String topic;
+	
+	private Vector<User> participants;
+
+	public ResearchProject(String topic, Vector<User> participants) {
+		this.topic = topic;
+		this.participants = participants;
+	}
+	public ResearchProject() {
+		
+	}
+
+	
+	//GET/SET Topic
+	public String getTopic() {
+		return topic;
+	}
+
+	public void setTopic(String topic) {
+		this.topic = topic;
+	}
+
+	//GTE/SET participants 
+	public Vector<User> getParticipants() {
+		return participants;
+	}
+
+
+	public void setParticipants(Vector<User> participants) {
+		this.participants = participants;
+	}
+
+	public void join(User user) {
+		if(participants.equals(null)) {
+			participants = new Vector<User>();
+		}
+		participants.add(user);
+	}
+
+
+
+	public int hashCode() {
+		return Objects.hash(participants, topic);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ResearchProject other = (ResearchProject) obj;
+		return Objects.equals(participants, other.participants) && Objects.equals(topic, other.topic);
+	}
+	
+	
+    
+    
+    
+}
